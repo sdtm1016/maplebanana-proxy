455db0fb5d59737c4ee02f82ff09ad642f74ceea
wushungzilong <wushuangzilong@gmail.com>
2013-11-21 20:10:48 +0800

    Signed-off-by: wushungzilong <wushuangzilong@gmail.com>

--- banana.C
+++ banana.C
@@ -1,7 +1,7 @@
 unit Unit1;
 
 interface
-// Maple Banana recourse ok£¡£¡
+// Maple Banana recourse ok£¡£¡¡¢
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs,IniFiles, StrUtils, StdCtrls, ExtCtrls,shellapi,TLhelp32, jpeg,
